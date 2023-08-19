ea6d1b034b5c4c359bb708aa31e306df4dcee297
Paulo Henrique Barbosa De Lima <ph.blima30@gmail.comm>
2023-08-19 00:59:41 -0300

    Todo-List frontend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'todo-list-angular';
+  title = 'Todo-list-angular';
+  headerTitle: string = 'Todo list';
 }
