995502af0eb2dc1a25bd802335c6d1918b0ca43a
Paulo Henrique Barbosa De Lima <ph.blima30@gmail.comm>
2023-08-20 22:17:17 -0300

    error404

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Todo-list-angular';
-  headerTitle: string = 'Todo list';
+    title = 'Todo-list-angular';
+    headerTitle: string = 'Todo list';
 }
